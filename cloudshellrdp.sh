c6af37e3f28ec7889443f45012d5f75ed0546063
nbalive2k <88097321+nbalive2k@users.noreply.github.com>
2021-10-11 13:24:28 -0300

    Update cloudshellrdp.sh

--- cloudshellrdp.sh
+++ cloudshellrdp.sh
@@ -4,8 +4,8 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/nu
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp 3389 &>/dev/null &
-./ngrok tcp 3389 &>/dev/null &
+nohup ./ngrok tcp 3389 --region=sa &>/dev/null &
+./ngrok tcp 3389 --region=sa &>/dev/null &
 echo Downloading File From akuh.net
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F > /dev/null 2>&1
 sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main" > /dev/null 2>&1
